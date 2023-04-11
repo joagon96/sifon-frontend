5850703d972f4933da6ce63199ec4e03fd75fdbb
joagon96 <joagon96@gmail.com>
2023-04-11 13:54:39 -0300

    fix api url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export const Config = Object.freeze({
-    // API_URL: 'http://0.0.0.0:5000/', //Docker
+    API_URL: 'http://127.0.0.1:5000/', //Docker
     // API_URL: 'http://192.168.0.15:80/' //Pruebas locales
-    API_URL: 'http://172.18.0.2:5000/'
 })
