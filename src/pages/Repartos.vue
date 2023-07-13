1d5ff74c76dd7e8ee33817d4fb34c8511f7bbb7a
joagon96 <joagon96@gmail.com>
2023-07-12 22:51:34 -0300

    add search repartos

--- src/pages/Repartos.vue
+++ src/pages/Repartos.vue
@@ -7,6 +7,7 @@
                 <div class="md-size-100">  
                   <h4 class="title md-size-80">Repartos
                     <md-button class="md-raised md-info md-size-20" style="float: right; margin-top: 10px" @click="openDialog()">Agregar</md-button>
+                    <md-button class="md-raised md-info md-size-20" style="float: right; margin-top: 10px; margin-right: 20px;" @click="showSearchDialog()"><md-icon>tune</md-icon> Filtros</md-button>
                   </h4>
                 </div>
                 </md-card-header>
@@ -86,6 +87,45 @@
                   </md-dialog>
                 </div>
             </md-card>
+
+            <div>
+              <md-dialog :md-active.sync="searchDialog" style="z-index: 5;">
+                <md-dialog-title>Filtrar</md-dialog-title>
+                <div class="md-layout-item md-small-size-100 md-size-100">
+                  <div class="md-layout md-gutter">
+                    <div class="md-layout-item">
+                      <md-field>
+                        <label>Zona</label>
+                        <md-select md-dense v-model="search.idZona">
+                          <md-option v-for="z in zonas" :key="z.idZona" :value="z.idZona">{{z.descripcion}}</md-option>
+                        </md-select>
+                      </md-field>
+                    </div>
+                    <div class="md-layout-item md-size-100">
+                          <md-field>
+                            <label>Dia</label>
+                            <md-select md-dense v-model="search.dia">
+                              <md-option v-for="d in dias" :key="d.id" :value="d.name">{{d.name}}</md-option>
+                            </md-select>
+                          </md-field>
+                        </div>
+                    <div class="md-layout-item md-size-100">
+                          <md-field>
+                            <label>Repartidor</label>
+                            <md-select md-dense v-model="search.idRepartidor">
+                              <md-option v-for="r in repartidores" :key="r.idRepartidor" :value="r.idRepartidor">{{r.nomapeRep}}</md-option>
+                            </md-select>
+                          </md-field>
+                        </div>
+                  </div>
+                </div>
+                <md-dialog-actions>
+                  <md-button @click="cleanSearch()">Limpiar filtros</md-button>
+                  <md-button class="md-primary" @click="searchRepartos()">Aplicar filtros</md-button>
+                </md-dialog-actions>
+              </md-dialog>
+            </div>
+
         </div>
       <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100">
       </div>
@@ -125,6 +165,12 @@ export default {
       ],
       repartidores: [],
       zonas: [],
+      searchDialog: false,
+      search:{
+        idZona: '',
+        dia: '',
+        idRepartidor: '',
+      }
     };
   },
   methods: {
@@ -230,6 +276,25 @@ export default {
         this.repartidores = Object.freeze(response.data)
       });
     },
+    searchRepartos() {
+      var bodyFormData = new FormData();
+      bodyFormData.append('idZona', this.search.idZona) ;
+      bodyFormData.append('dia', this.search.dia) ;
+      bodyFormData.append('idRepartidor', this.search.idRepartidor) ;
+      axios({
+        method: 'POST',
+        url: Config.API_URL + 'search/reparto',
+        data: bodyFormData,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": "true"
+        }
+      }).then(response => {
+        this.searchDialog = false
+        this.repartos = response.data;
+      });
+    },
     cleanReparto(){
       this.newReparto.idReparto = null
       this.newReparto.idZona = null
@@ -244,6 +309,15 @@ export default {
       this.cleanReparto()
       this.isEdit = false
       this.showDialog = true
+    },
+    cleanSearch(){
+      this.search.idZona = ''
+      this.search.dia = ''
+      this.search.idRepartidor = ''
+    },
+    showSearchDialog(){
+      this.cleanSearch()
+      this.searchDialog = true
     }
   }
 };
