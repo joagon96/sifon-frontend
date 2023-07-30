adab40ce2e3c9a067caba73265004b61db27d60f
joagon96 <joagon96@gmail.com>
2023-07-30 16:56:05 -0300

    change cofig

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   devServer: {
+    disableHostCheck: true,
     proxy: "http://192.168.0.17:80",
     public: '0.0.0.0:8080',
     hot: true,
-    disableHostCheck: true,
-    allowedHosts: [
-      'localhost',
-      'sifon-fronted.onrender.com',
-    ],
   }
 };
